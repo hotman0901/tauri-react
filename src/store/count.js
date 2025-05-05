0046940b6bf0ac35efa5d686f4b65f1cbdca32f7
bennyhsieh <benny.hsieh@jyintgroup.com>
2025-05-05 17:05:46 +0800

    feat: update

--- src/store/count.js
+++ src/store/count.js
@@ -1,13 +1,28 @@
 import { create } from "zustand";
-import { immer } from "zustand/middleware/immer";
+// import { immer } from "zustand/middleware/immer";
+import { mutative } from "zustand-mutative";
+
+// export const useBearStore = create(
+//   immer((set) => ({
+//     count: 1,
+//     increase: () => {
+//       set((state) => {
+//         state.count += 1;
+//       });
+//     },
+//   })),
+// );
 
 export const useBearStore = create(
-  immer((set) => ({
-    count: 1,
-    increase: () => {
+  mutative((set) => ({
+    count: 0,
+    increase: (qty) =>
       set((state) => {
-        state.count += 1;
-      });
-    },
+        state.count += qty || 1;
+      }),
+    decrement: (qty) =>
+      set((state) => {
+        state.count -= qty;
+      }),
   })),
 );
